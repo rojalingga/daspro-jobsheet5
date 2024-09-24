0dc6e28c7d9f40d29bd1348504db49475afe119d
rojalingga <rojalingga2006@gmail.com>
2024-09-24 20:15:39 +0700

    Sesudah Revisi

--- PemilihanBilangan20.java
+++ PemilihanBilangan20.java
@@ -6,6 +6,7 @@ public class PemilihanBilangan20 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        
 
         System.out.print("Masukkan sebuah angka: ");
         int angka = sc.nextInt();
