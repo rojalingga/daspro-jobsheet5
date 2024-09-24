2f877c217e34a51bd1898801ced060ecff51fdcb
rojalingga <rojalingga2006@gmail.com>
2024-09-24 20:20:36 +0700

    Sesudah Revisi fix

--- PemilihanBilangan20.java
+++ PemilihanBilangan20.java
@@ -6,15 +6,14 @@ public class PemilihanBilangan20 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
 
         System.out.print("Masukkan sebuah angka: ");
-        int angka = sc.nextInt();
+        int angka = sc.nextInt(); 
 
-        if (angka % 2 == 0) {
-            System.out.println(angka + " termasuk bilangan genap");
-        } else {
-            System.out.println(angka + " termasuk bilangan ganjil");
-        }
+
+        // Menggunakan operator ternary
+        String hasil = (angka % 2 == 0) ? "genap" : "ganjil";
+
+        System.out.println(angka + " termasuk bilangan " + hasil);
     }
 }
